cf75b14699efb779796288be1c3f7739a27bbcd7
Abdul Hai Noor <noor.companyofficial@gmail.com>
2026-05-05 18:44:30 +0100

    update app js security settings

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.use(cookieParser());
 // ====== Session ======
 app.use(
   session({
-    secret: process.env.SESSION_SECRET
+    secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
   }),
